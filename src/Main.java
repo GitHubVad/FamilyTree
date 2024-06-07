892e249e79bcdb17288f8880c935989b4b19d026
GitHubVad <onwww@yandex.ru>
2024-06-07 22:12:04 +0300

    Family Tree

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import model.FamilyTreeService;
+import model.familyTree.FamilyTree;
+import model.human.Gender;
+import model.human.Human;
+import model.save.FileHandlerForFamilyTree;
+import view.Console;
+import view.View;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        FamilyTree<Human> tree = new FamilyTree<>();
+
+        Human vasya = new Human("Василий", Gender.Male, LocalDate.of(1963, 12, 10));
+        Human masha = new Human("Мария", Gender.Female, LocalDate.of(1965, 9, 15));
+        Human christina = new Human("Кристина", Gender.Female, LocalDate.of(1988, 7, 5),
+                vasya, masha);
+        Human semyon = new Human("Семен", Gender.Male, LocalDate.of(1991, 1, 25),
+                vasya, masha);
+        Human vadim = new Human("Вадим", Gender.Male, LocalDate.of(1983, 7, 25));
+        Human tamara = new Human("Тамара", Gender.Female, LocalDate.of(1949, 10, 14));
+
+        tree.add(vasya);
+        tree.add(masha);
+        tree.add(christina);
+        tree.add(semyon);
+        tree.add(vadim);
+        tree.add(tamara);
+
+        Human grandMother = new Human("Лариса", Gender.Female, LocalDate.of(1945, 9, 1));
+        grandMother.addChild(vasya);
+
+        tree.add(grandMother);
+
+        System.out.println(tree);
+
+        FamilyTreeService service = new FamilyTreeService(tree);
+        service.setWritable(new FileHandlerForFamilyTree());
+        service.save();
+
+        View view = new Console();
+        view.start();
+    }
+}
